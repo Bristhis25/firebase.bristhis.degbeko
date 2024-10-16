f50a807feafc5b5c7e5ac0536450c92527dc99d5
Bristhis25 <bristhisdegbeko@gmail.com>
2024-10-16 14:27:21 +0200

    fist commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import 'dotenv/config';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+console.log("Bonjour tout le monde v1");
+
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  };
+ 
+
+ const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
+
+ const getfactures = async (db) => {
+    const facturesCol = collection(db, 'factures');
+    const facturesSnapshot = await getDocs(facturesCol);
+    const factures = facturesSnapshot.docs.map(doc => ({...doc.data(), id: doc.id}));
+    return factures;
+  }
+  const factures = await getfactures(db);
+  console.log(factures)
+ 
